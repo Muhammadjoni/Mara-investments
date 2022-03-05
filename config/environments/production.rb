2323f3df3b15c88fe6460e99dcad6bf816f963df
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2022-03-06 02:05:42 +0400

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.blockchain_api_key = ENV.fetch('BLOCKCHAIN_API_KEY')
 end
